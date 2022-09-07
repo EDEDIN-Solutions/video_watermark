45d7964f27e9ceaf65fe7f97a6475d8bf2604aa7
Sugan P <sugan.dev694@gmail.com>
2022-09-07 18:58:36 +0530

    Added assert to trim video model

--- lib/src/trim_video.dart
+++ lib/src/trim_video.dart
@@ -2,5 +2,7 @@ class TrimVideo {
   final Duration start;
   final Duration end;
   Duration get duration => end - start;
-  TrimVideo({required this.start, required this.end});
+  TrimVideo({required this.start, required this.end})
+      : assert(end < start,
+            "Start time should be less than the end time to trim the video");
 }
